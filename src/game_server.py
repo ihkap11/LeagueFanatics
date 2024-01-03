4425f7c9bc3443f8ec5fe8687a893e362418494d
ihkap11 <bpakhi97@gmail.com>
2024-01-03 05:08:44 +0000

    Add: Game and Matchmaking class
    Game class:
    - maintain queue
    - request match
    - update_player_stats [TODO]
    
    Mathmaking Class:
    - simulate match
    - launch practice game [WIP]

---
+++ src/game_server.py
@@ -0,0 +1,77 @@
+from player import Player, player_db
+from team import Team
+import random
+from typing import List
+from skill import StatsCalculator
+
+
+class Game:
+    def __init__(self):
+        self.matchmaker = Matchmaking("practice")
+        self.ranked_queue = []
+
+    def __is_queue_ready(self, queue: List):
+        return len(queue) > 10
+
+    def request_match(self, player: Player, mode: str):
+        if mode == "ranked":
+            self.ranked_queue.append(player)
+            if self.__is_queue_ready(self.ranked_queue):
+                outcome = self.matchmaker.simulate_match(player, mode)
+        else:
+            outcome = self.matchmaker.simulate_match(player, mode)
+
+    def update_player_stats(self):
+        return
+
+
+class Matchmaking:
+    def __init__(self, mode: str, team_size: int = 5):
+        self.mode = mode
+        self.team_size = team_size
+
+    def __set_match_mode(self, mode):
+        self.mode = mode
+
+    def __create_bot_team(self, player: Player, all_bots: bool = False):
+        skill = player.skill
+        uncertainty = player.uncertainty
+        team_size = self.team_size
+
+        if all_bots:
+            players = []
+        else:
+            players = [player]
+            team_size -= team_size
+
+        for i in range(team_size):
+            bot_player = Player(name=f"bot_{i}", skill=skill, uncertainty=uncertainty)
+            players.append(bot_player)
+        return Team(players=players)
+
+    def __match_outcome(self, team1: Team, team2: Team):
+        if random.choice([True, False]):
+            team1.wins = True
+        else:
+            team2.wins = False
+
+    def simulate_match(self, player: Player, mode: str):
+        self.__set_match_mode(mode=mode)
+        if mode == "practice":
+            return self.launch_practice_game(player)
+        elif mode == "ranked":
+            return self.launch_ranked_game(player)
+
+    def launch_practice_game(self, player: Player):
+        # create team for player
+        team1 = self.__create_bot_team(player)
+        team2 = self.__create_bot_team(player, all_bots=True)
+        self.__match_outcome(team1, team2)
+        updated_team1, updated_team2 = StatsCalculator.update_team_player_skills(
+            team1, team2
+        )
+
+        print(updated_team1)
+        print(updated_team2)
+
+        return True
