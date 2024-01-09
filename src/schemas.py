234a32eeb003b937ac5bacf491a5642e8e9c09ba
ihkap11 <bpakhi97@gmail.com>
2024-01-09 07:00:45 +0000

    update team

--- src/schemas.py
+++ src/schemas.py
@@ -1,7 +1,6 @@
 import uuid
 from pydantic import BaseModel, Field
 from typing import List, Optional
-from player import Player
 
 
 class PlayerData(BaseModel):
@@ -11,14 +10,3 @@ class PlayerData(BaseModel):
     uncertainty: Optional[float] = 0.0
     level: Optional[int] = 0
     role_preference: Optional[List[int]] = []
-
-
-class TeamData(BaseModel):
-    class Config:
-        arbitrary_types_allowed = True
-
-    players: List[Player] = []
-    size: int = 5
-    skill: Optional[float] = 0.0
-    uncertainty: Optional[float] = 0.0
-    wins: bool
